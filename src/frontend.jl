b611405efd6e467fda7948723456f4510f2f8138
Mirek Kratochvil <exa.exa@gmail.com>
2022-06-23 14:54:26 +0200

    add rule precedence generators

--- src/frontend.jl
+++ src/frontend.jl
@@ -121,6 +121,95 @@ Build a [`ZeroOrMore`](@ref) clause. Translate to strongly typed grammar with [`
 """
 zero_or_more(x) = ZeroOrMore(x)
 
+"""
+$(TYPEDSIGNATURES)
+
+Convert a list of rules of increasing associativity to a typical
+precedence-handling "failthrough" construction. The result must be
+post-processed by [`flatten`](@ref).
+
+Each of the rules is abstracted by "same-associativity" and
+"higher-associativity" rules (i.e., it is a binary function), which is used to
+correctly link the rules within the precedence group. The first rule is of the
+lowest precedence. All rules except the last automatically fallback to the next
+rule. The higher-precedence parameter of the last rule is the label of the
+first rule.
+
+`label` is a function that generates the label for given `n`-th level of the grammar.
+
+Use [`@precedences`](@ref) for a less verbose construction.
+
+Returns a vector of labeled rules; that must usually be interpolated into the
+ruleset.
+
+# Example
+
+    Dict(
+        precedence_cascade(
+            n -> Symbol(:exprlevel, n),
+            (same, next) -> :expr => first(
+                :plus => seq(same, token('+'), next),
+                :minus => seq(same, token('-'), next),
+            ),
+            (same, next) -> :times => seq(same, token('*'), next), # left associative
+            (same, next) -> :power => seq(next, token('^'), same), # right associative
+            (_, restart) -> first(
+                :parens => seq(token('('), restart, token(')')),
+                :digits => one_or_more(satisfy(isdigit)),
+            ),
+        )...,
+    )
+"""
+function precedence_cascade(label::Function, levels...)
+    n = length(levels)
+    n > 0 || return first() # always fail if there are no rules
+    precs = label.(1:n)
+
+    return [
+        precs[i] => first(levels[i](precs[i], precs[1+i%n]), precs[1+i%n]) for
+        i in eachindex(levels)
+    ]
+end
+
+"""
+    @precedences labeller same::Symbol next::Symbol rules
+
+A shortcut macro for [`precedence_cascade`](@ref). Automatically adds lambda
+heads with fixed argument names, and splats itself with `...` into the
+surrounding environment.
+
+# Example
+
+    Dict(
+        @precedences (n->Symbol(:exprlevel, n)) same next begin
+            :expr => seq(same, token('+'), next)
+            seq(same, token('*'), next)
+            first(
+                token('x'),
+                seq(token('('), next, token(')'))
+            )
+        end
+    )
+"""
+macro precedences(labeller, same::Symbol, next::Symbol, block)
+    block.head == :block ||
+        throw(DomainError(block, "@precedences expects a block of grammar rules"))
+    esc(
+        Expr(
+            :...,
+            Expr(
+                :call,
+                precedence_cascade,
+                labeller,
+                [
+                    Expr(:->, Expr(:tuple, same, next), r) for
+                    r = block.args if !(r isa LineNumberNode)
+                ]...,
+            ),
+        ),
+    )
+end
+
 
 """
 $(TYPEDSIGNATURES)
@@ -140,16 +229,18 @@ convenience rule building functions:
 - [`followed_by`](@ref)
 - [`one_or_more`](@ref)
 - [`zero_or_more`](@ref)
+- [`precedence_cascade`](@ref) (not backed by an actual [`Clause`](@ref)!)
 
-Anonymous nested rules are assigned names that are constructed by `childname`
+Anonymous nested rules are assigned names that are constructed by `childlabel`
 function (gets the original G and and integer with position integer). By
-default, `childname` concatenats the parent rule name, hyphen, and the position
-number to form a `Symbol` (i.e., works only in cases when the rules are indexed
-by symbols).
+default, `childlabel` concatenats the parent rule name, hyphen, and the position
+number to form a `Symbol` (i.e., the default works only in cases when the rules
+are labeled by Symbols, and you need to provide your own implementation for
+other grammars labeled e.g. by integers or strings).
 """
 function flatten(
     rules::Dict{G},
-    childname::Function = (rid, idx) -> Symbol(rid, :-, idx),
+    childlabel::Function = (rid, idx) -> Symbol(rid, :-, idx),
 )::Dict{G,Clause{G}} where {G}
     todo = Pair{G,Clause}[r for r in rules]
     res = Dict{G,Clause{G}}()
@@ -167,7 +258,7 @@ function flatten(
                     if ch isa G
                         ch
                     elseif ch isa Clause
-                        newsym = childname(rid, i)
+                        newsym = childlabel(rid, i)
                         push!(todo, newsym => ch)
                         newsym
                     elseif ch isa Pair && Base.first(ch) isa G && last(ch) isa Clause
