a460a99a1d7ef81c9ba1e71f35f7d893a202334b
Mirek Kratochvil <exa.exa@gmail.com>
2022-11-23 21:15:11 +0100

    add lexing tests

--- src/parse.jl
+++ src/parse.jl
@@ -203,7 +203,7 @@ parsing terminals where another terminal was already parsed successfully.
 function lex(g::Grammar{G,T}, input::I)::Vector{Vector{Tuple{G,Int}}} where {G,T,I}
     q = PikaQueue(lastindex(input))
     push!(q, 1)
-    res = fill(Vector{Tuple{G,Int}}(), lastindex(input))
+    res = [Vector{Tuple{G,Int}}() for _ = 1:lastindex(input)] # tricky: do not fill()
     while !isempty(q)
         pos = pop!(q)
         for tidx in g.terminals
--- test/fastmatch.jl
+++ test/fastmatch.jl
@@ -21,3 +21,39 @@
     )
     @test x == [123, 234, 345]
 end
+
+@testset "Lexing" begin
+    rules = Dict(
+        :digits => P.scan(m -> begin
+            i = firstindex(m)
+            while i <= lastindex(m) && isdigit(m[i])
+                i = nextind(m, i)
+            end
+            i > firstindex(m) ? i - firstindex(m) : -1
+        end),
+        :seq => P.seq(:digits, P.many(:cont => P.seq(:sep => P.token(','), :digits))),
+    )
+
+    g = P.make_grammar([:seq], P.flatten(rules, Char))
+    input = "123,234,345"
+
+    # lexing shouldn't make too much tokens
+    @test sum(length.(P.lex(g, input))) == 5
+
+    p = P.parse_lex(g, input)
+
+    # consequently parsing should generate a smaller match table
+    @test length(p.matches) <= 15 # (this is tight)
+
+    mid = P.find_match_at!(p, :seq, 1)
+    @test p.matches[mid].len == length(input)
+
+    x = P.traverse_match(
+        p,
+        mid,
+        fold = (m, p, s) ->
+            m.rule == :digits ? parse(Int, String(m.view)) :
+            m.rule == :seq ? [s[1], s[2]...] : m.rule == :cont ? s[2] : s,
+    )
+    @test x == [123, 234, 345]
+end
